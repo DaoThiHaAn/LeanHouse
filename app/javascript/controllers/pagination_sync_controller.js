e5cd5c5f2c39f9832c17d225dc5fe65c7aec1a14
Dao Thi Ha An <daothihaan@gmail.com>
2026-08-09 23:35:25 +0700

    pagination fix

--- app/javascript/controllers/pagination_sync_controller.js
+++ app/javascript/controllers/pagination_sync_controller.js
@@ -1,28 +1,51 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.syncUrlFromFrame()
+  static values = {
+    canonicalUrl: String, // URL to display in the browser
+    defaultParams: Object, // fixed default
+    queryParams: Array, // request params to preserve
+    pageParam: { type: String, default: "page" }, // optional, defaults to "page"
+    totalPagesSelector: String // data-pagination-total-pages
   }
 
-  syncUrlFromFrame() {
-    const frame = this.element
-    const currentUrl = new URL(window.location.href)
-    const pageParam = currentUrl.searchParams.get("page")
-
-    if (pageParam) {
-      frame.setAttribute("data-page", pageParam)
-    }
+  connect() {
+    console.log("Pagination controller connected")
   }
 
   updateUrl(event) {
     const frame = event.target
-    const page = frame?.getAttribute("data-page") || new URL(frame.src || window.location.href).searchParams.get("page")
+    const responseUrl = event.detail?.fetchResponse?.response?.url || frame.src
+    if (!responseUrl) return
 
-    if (page) {
-      const currentUrl = new URL(window.location.href)
-      currentUrl.searchParams.set("page", page)
-      window.history.replaceState({}, "", currentUrl)
+    const frameUrl = new URL(responseUrl, window.location.origin)
+    const canonicalUrl = new URL(this.canonicalUrlValue, window.location.origin)
+    const totalPagesElement = this.hasTotalPagesSelectorValue && frame.querySelector(this.totalPagesSelectorValue)
+    const totalPages = Number(totalPagesElement?.dataset.paginationTotalPages)
+
+    const parameterNames = new Set([
+      ...this.queryParamsValue,
+      ...Object.keys(this.defaultParamsValue)
+    ])
+
+    parameterNames.forEach((name) => {
+      const value = frameUrl.searchParams.get(name)
+      const defaultValue = this.defaultParamsValue[name]
+
+      if (value) {
+        canonicalUrl.searchParams.set(name, value)
+      } else if (Object.hasOwn(this.defaultParamsValue, name)) {
+        canonicalUrl.searchParams.set(name, defaultValue)
+      }
+    })
+
+    const page = frameUrl.searchParams.get(this.pageParamValue)
+    if (totalPages > 1) {
+      canonicalUrl.searchParams.set(this.pageParamValue, page || "1")
     }
+
+    window.history.replaceState(window.history.state, "", canonicalUrl)
+
+    console.log("Update url")
   }
 }
