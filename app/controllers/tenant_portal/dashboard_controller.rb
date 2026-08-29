16368e78530199ec045168fd946d5daf1097cf3e
Dao Thi Ha An <daothihaan@gmail.com>
2026-08-30 02:22:11 +0700

    fix: CI fails

--- app/controllers/tenant_portal/dashboard_controller.rb
+++ app/controllers/tenant_portal/dashboard_controller.rb
@@ -3,9 +3,9 @@ class TenantPortal::DashboardController < TenantPortal::BaseController
     @contract = @tenant_stay&.contract || @tenant.latest_contract
 
     if @contract&.start_date && @contract&.due_date
-      @days_stayed = [(Date.current - @contract.start_date).to_i, 0].max
-      @total_days = [(@contract.due_date - @contract.start_date).to_i, 1].max
-      @stay_progress_pct = [((@days_stayed.to_f / @total_days) * 100).round, 100].min
+      @days_stayed = [ (Date.current - @contract.start_date).to_i, 0 ].max
+      @total_days = [ (@contract.due_date - @contract.start_date).to_i, 1 ].max
+      @stay_progress_pct = [ ((@days_stayed.to_f / @total_days) * 100).round, 100 ].min
     else
       @days_stayed = 0
       @total_days = 0
